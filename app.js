e060236363ab350546d4c9dcfa7842dfc9ceda92
rahulrengesh <80974308+rahulrengesh@users.noreply.github.com>
2021-06-20 18:41:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const countdown = () => {
+    const countDate = new Date('September 12, 2021 00:00:00').getTime();
+    const preDate= new Date().getTime();
+    const gap=countDate-preDate;
+    console.log(countDate);
+    const second=1000;
+    const minute=second*60;
+    const hour=minute*60;
+    const day = hour*24;
+
+    const textDay= Math.floor(gap/day);
+    const textHour = Math.floor((gap% hour)/hour);
+    const textMinute = Math.floor((gap% hour)/minute);
+    const textSecond = Math.floor((gap% hour)/second);
+
+    document.querySelector(".day").innerText = textDay;
+    document.querySelector(".hour").innerText = textHour;
+    document.querySelector(".minute").innerText = textMinute;
+    document.querySelector(".second").innerText = textSecond;
+};
+setInterval(countdown,1000);
